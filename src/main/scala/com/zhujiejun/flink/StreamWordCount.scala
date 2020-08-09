1c74b865ca617a61184491d18fe89b5302631733
zhujiejun <2383322468@qq.com>
2020-08-09 16:44:37 +0800

    add StreamWordCount

---
+++ src/main/scala/com/zhujiejun/flink/StreamWordCount.scala
@@ -0,0 +1,19 @@
+package com.zhujiejun.flink
+
+import org.apache.flink.streaming.api.scala._
+
+object StreamWordCount {
+
+    def main(args: Array[String]): Unit = {
+        val env = StreamExecutionEnvironment.getExecutionEnvironment
+        val dataStream = env.socketTextStream("localhost", 18888)
+        val wordCountDataStream = dataStream.flatMap(_.split(" "))
+            .filter(_.nonEmpty)
+            .map((_, 1))
+            .keyBy(0)
+            .sum(1)
+        wordCountDataStream.print()
+
+        env.execute("stream word count job")
+    }
+}
