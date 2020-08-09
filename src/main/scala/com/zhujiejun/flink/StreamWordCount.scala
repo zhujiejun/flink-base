cf2cf78f464506ae2646b88a3b7cb9ac179fcbcf
zhujiejun <2383322468@qq.com>
2020-08-09 17:07:08 +0800

    format code

--- src/main/scala/com/zhujiejun/flink/StreamWordCount.scala
+++ src/main/scala/com/zhujiejun/flink/StreamWordCount.scala
@@ -3,7 +3,6 @@ package com.zhujiejun.flink
 import org.apache.flink.streaming.api.scala._
 
 object StreamWordCount {
-
     def main(args: Array[String]): Unit = {
         val env = StreamExecutionEnvironment.getExecutionEnvironment
         val dataStream = env.socketTextStream("localhost", 18888)
