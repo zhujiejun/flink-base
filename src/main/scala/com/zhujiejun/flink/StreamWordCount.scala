2a4c5d0a64b91a0f5819a3e9afb8e8601a624314
zhujiejun <2383322468@qq.com>
2020-08-10 17:18:06 +0800

    modify shadow conf

--- src/main/scala/com/zhujiejun/flink/StreamWordCount.scala
+++ src/main/scala/com/zhujiejun/flink/StreamWordCount.scala
@@ -1,11 +1,16 @@
 package com.zhujiejun.flink
 
+import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.scala._
 
 object StreamWordCount {
     def main(args: Array[String]): Unit = {
+        val param = ParameterTool.fromArgs(args)
+        val host = param.get("host")
+        val port = param.get("port")
+
         val env = StreamExecutionEnvironment.getExecutionEnvironment
-        val dataStream = env.socketTextStream("localhost", 18888)
+        val dataStream = env.socketTextStream(host, port.toInt)
         val wordCountDataStream = dataStream.flatMap(_.split(" "))
             .filter(_.nonEmpty)
             .map((_, 1))
